daadf18c0b3375ee38a01136d34ebb9735169012
Nursultan Begaliev <nursultanbegaliev1@gmail.com>
2021-01-15 12:15:15 +0600

    music added

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
+let audioA = new Audio("white_keys/A.mp3");
+let audioS = new Audio("white_keys/S.mp3");
+let audioD = new Audio("white_keys/D.mp3");
+let audioF = new Audio("white_keys/F.mp3");
+let audioG = new Audio("white_keys/G.mp3");
+let audioH = new Audio("white_keys/H.mp3");
+let audioJ = new Audio("white_keys/J.mp3");
+let audioW = new Audio("black_keys/W.mp3");
+let audioE = new Audio("black_keys/E.mp3");
+let audioT = new Audio("black_keys/T.mp3");
+let audioY = new Audio("black_keys/Y.mp3");
+let audioU = new Audio("black_keys/U.mp3");
+
 
 document.addEventListener("keydown", function(event) {
   if(event.key == "a") {
       console.log("The 'A' key is pressed");
-      let audioA = new Audio("white_keys/A.mp3");
+      
       audioA.load();
       audioA.play();
 
@@ -11,7 +24,7 @@ document.addEventListener("keydown", function(event) {
 document.addEventListener("keydown", function(event){
   if(event.key == "s") {
       console.log("The 'S' key is pressed");
-      let audioS = new Audio("white_keys/S.mp3");
+      
       audioS.load();
       audioS.play();
   }
@@ -19,7 +32,7 @@ document.addEventListener("keydown", function(event){
 document.addEventListener("keydown", function(event) {
   if(event.key == "d") {
       console.log("The 'D' key is pressed");
-      let audioD = new Audio("white_keys/D.mp3");
+      
       audioD.load();
       audioD.play();
   }
@@ -27,7 +40,7 @@ document.addEventListener("keydown", function(event) {
 document.addEventListener("keydown", function(event) {
   if(event.key == "f") {
       console.log("The 'F' key is pressed");
-      let audioF = new Audio("white_keys/F.mp3");
+      
       audioF.load();
       audioF.play();
   }
@@ -35,7 +48,7 @@ document.addEventListener("keydown", function(event) {
 document.addEventListener("keydown", function(event) {
   if(event.key == "g") {
       console.log("The 'G' key is pressed");
-      let audioG = new Audio("white_keys/G.mp3");
+      
       audioG.load();
       audioG.play();
   }
@@ -43,7 +56,7 @@ document.addEventListener("keydown", function(event) {
 document.addEventListener("keydown", function(event) {
   if(event.key == "h") {
       console.log("The 'H' key is pressed");
-      let audioH = new Audio("white_keys/H.mp3");
+      
       audioH.load();
       audioH.play();
   }
@@ -51,7 +64,7 @@ document.addEventListener("keydown", function(event) {
 document.addEventListener("keydown", function(event) {
   if(event.key == "j") {
       console.log("The 'J' key is pressed");
-      let audioJ = new Audio("white_keys/J.mp3");
+     
       audioJ.load();
       audioJ.play();
   }
@@ -61,27 +74,27 @@ document.addEventListener("keydown", function(event){
   if(event.key == "w" || event.key == "e" || event.key == "t" || event.key == "y" || event.key == "u" ) {
       if(event.key == "w"){
           console.log("The 'W' key is pressed");
-          let audioW = new Audio("black_keys/W.mp3");
+          
           audioW.load();
           audioW.play();
       }
       else if(event.key == "e") {
-          let audioE = new Audio("black_keys/E.mp3");
+          
           audioE.load();
           audioE.play();
       }
       else if(event.key == "t"){
-          let audioT = new Audio("black_keys/T.mp3");
+          
           audioT.load();
           audioT.play();
       }
       else if(event.key == "y") {
-          let audioY = new Audio("black_keys/Y.mp3");
+          
           audioY.load();
           audioY.play();
       }
       else if(event.key == "u") {
-          let audioU = new Audio("black_keys/U.mp3");
+          
           audioU.load();
           audioU.play();
       }
